6d7095c4cba1b3b8dff068b0a606c34d20a067b6
David Ceballos <maildceballos@gmail.com>
2021-05-25 15:03:18 -0300

    index.html actualizado ver.3

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 
 export const getGifs = async ( category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=rXDJx1bFdKDv8C7B5wNN3B8qg0d5zfp4`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=rXDJx1bFdKDv8C7B5wNN3B8qg0d5zfp4`
     const resp = await fetch( url );
     const { data } = await resp.json();
 
